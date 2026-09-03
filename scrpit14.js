e683545e0707357587de4aa707824443af6dc81a
Adalberto Marinho <adalbertomarinho2010@gmail.com>
2026-09-03 12:05:17 -0400

    Criar scrpit14.js

---
+++ scrpit14.js
@@ -0,0 +1,10 @@
+let nome = "Adalberto";
+let idade = 16;
+let curso = "Programação";
+let cidade = "Campo Grande";
+
+let frase1 = "Meu nome é " + nome + ", tenho " + idade + " anos, estudo " + curso + " e moro em " + cidade + ".";
+console.log(frase1);
+
+let frase2 = `Meu nome é ${nome}, tenho ${idade} anos, estudo ${curso} e moro em ${cidade}.`;
+console.log(frase2);
