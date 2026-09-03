97c9bf0b70e0060d54c5666442dc78ffc762f8c8
Adalberto Marinho <adalbertomarinho2010@gmail.com>
2026-09-03 12:05:37 -0400

    Criar script15.js

---
+++ script15.js
@@ -0,0 +1,6 @@
+let nome = prompt("Qual o seu nome?");
+let idade = prompt("Qual a sua idade?");
+let curso = prompt("Qual o seu curso?");
+let cidade = prompt("Qual a sua cidade?");
+
+alert(`Olá, ${nome}. Você tem ${idade} anos, estuda ${curso} e mora em ${cidade}.`);
