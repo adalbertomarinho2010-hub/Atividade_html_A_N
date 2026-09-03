9288969c3bdf1cde954b7a19f831b59db8f1c388
Adalberto Marinho <adalbertomarinho2010@gmail.com>
2026-09-03 12:07:38 -0400

    Criar script17.js

---
+++ script17.js
@@ -0,0 +1,7 @@
+let salario = Number(prompt("Digite o salário atual:"));
+let aumento = salario * 0.10;
+let novo_salario = salario + aumento;
+
+console.log("Salário atual: " + salarioAtual);
+console.log("Aumento: " + aumento);
+console.log("Novo salário: " + novo_salario);
