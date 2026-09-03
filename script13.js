b7885d52147d848a8e953a63312771af3411b87b
Adalberto Marinho <adalbertomarinho2010@gmail.com>
2026-09-03 12:04:35 -0400

    Criar script13.js

---
+++ script13.js
@@ -0,0 +1,5 @@
+let nota = 40.55;
+
+console.log("Valor original: " + nota);
+console.log("Duas casas decimais: " + nota.toFixed(2));
+console.log("Substituindo ponto por vírgula: " + nota.toFixed(2).replace(".", ","));
