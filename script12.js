eeba233c0a701f34187e50c51bf54f843b7fd673
Adalberto Marinho <adalbertomarinho2010@gmail.com>
2026-09-03 12:04:05 -0400

    Criar script12.js

---
+++ script12.js
@@ -0,0 +1,10 @@
+if (true) {
+    let cidade = "Campo Grande";
+}
+
+console.log(cidade);
+
+/* 
+ O erro ocorre porque a variável "let" só existe dentro do bloco 
+ onde foi criada (dentro das chaves do if). Fora dele, ela não é reconhecida.
+*/
