0e3afa72a8612c9ca7656f9726edfee5bd78fe14
Adalberto Marinho <adalbertomarinho2010@gmail.com>
2026-09-03 12:06:10 -0400

    Criar script16.js

---
+++ script16.js
@@ -0,0 +1,9 @@
+let n1 = Number(prompt("Digite o primeiro número:"));
+let n2 = Number(prompt("Digite o segundo número:"));
+
+console.log("Soma: " + (n1 + n2));
+console.log("Subtração: " + (n1 - n2));
+console.log("Multiplicação: " + (n1 * n2));
+console.log("Divisão: " + (n1 / n2));
+console.log("Resto da divisão: " + (n1 % n2));
+console.log("Potência: " + (n1 ** n2));
