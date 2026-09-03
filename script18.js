9862dde31d07e9bd71ae63fff0d7f2c23460f779
Adalberto Marinho <adalbertomarinho2010@gmail.com>
2026-09-03 12:08:27 -0400

    Criar script18.js

---
+++ script18.js
@@ -0,0 +1,5 @@
+let idade = Number(prompt("Digite sua idade:"));
+
+console.log("Maior de idade? " + (idade >= 18));
+console.log("Menor de idade? " + (idade < 18));
+console.log("Tem exatamente 18 anos? " + (idade === 18));
